05ee22b25a5e2bfb90d1c4ab9a2f68469c157536
NegativeIndex <daiweitao@yahoo.com>
2019-11-03 01:12:21 -0500

    Current version is done

--- mkjob_comsol.py
+++ mkjob_comsol.py
@@ -7,19 +7,19 @@ import os,glob,sys,shutil,subprocess
 import re,random
 import logging,argparse
 
-def analyze_mph_files(idx):
+def analyze_mph_files(pick):
     """Analyze the \*.mph files and \*_Done.mph files in the current
     folder. Generate a to-do list for the next simulation. The
-    function returns a string based on idx.
+    function returns a string based on pick.
     
-      * idx=0: return the summary of the folder
+      * pick=0: return the summary of the folder
    
-      * idx>0: return a mph name chosen from the head of the to-do
+      * pick>0: return a mph name chosen from the head of the to-do
         list. The mph name is a string without mph extension. The
-        returned name is a random pick from the first idx unfinished
+        returned name is a random pick from the first pick unfinished
         names.
 
-      * idx<0: return a mph name chosen form the tail of the to-do list.
+      * pick<0: return a mph name chosen form the tail of the to-do list.
 
     """
     mphfiles=glob.glob("*mph")
@@ -54,34 +54,85 @@ def analyze_mph_files(idx):
                 todofiles.append(mph3)
     
     # retrun a string
-    
     m=len(todofiles)
-    if idx==0:
-        return "{} simulaitons; {} finished; {} werid".format(nt,nd,nb);
+    if pick is None: pick=0
+    if pick==0:
+        return "{} simulaitons; {} to do; {} finished; {} weird".format(
+            nt,nt-nd,nd,nb);
     elif m==0:  # todofiles is empety
         return ""
-    elif idx>0:  # random pick from the head
-        n=min(idx,m)
+    elif pick>0:  # random pick from the head
+        n=min(pick,m)
         return random.choice(todofiles[0:n])
     else:   # random pick from the tail
-        n=min(-idx,m)
-        return random.choice(todofiles[m+idx:m])
+        n=min(-pick,m)
+        return random.choice(todofiles[m+pick:m])
 
 #####
-def generate_job_file():
-    """ Generate the job file
+def generate_job_file(args):
+
+    """Generate the job file. We have to supply several parameters to the
+    job file: cpu, memory, queue and pick (analyze_mph_files parameter).
 
     """
     rfolder='/Users/wdai11/python-study/MKJob_Comsol'
+    jobname='./dwt-comsol.job'
     shutil.copy2(os.path.join(rfolder,'dwt-comsol-job-file.job'),
-                 './dwt-comsol.job')
+                 jobname)
     print('copy dwt-comsol.job')
-    with open('dwt-comsol.job', 'r') as f:
+    
+    # read the job file
+    with open(jobname, 'r') as f:
         jobfile=f.readlines()
 
+    # change job file
+    for idx, line in enumerate(jobfile):
+        if args.queue is not None:
+            if re.match('#\$ -q\s+([.\w]+)',line):
+                jobfile[idx]='#$ -q {}\n'.format(args.queue)
 
+        if args.core is not None:
+            if re.match('#\$ -pe\s+(\d+)cpn\s+(\d+)',line):
+                jobfile[idx]='#$ -pe {}cpn {}\n'.format(args.core,args.core)
+            if re.match('ncore=(\d+)',line):
+                jobfile[idx]='ncore={}\n'.format(round(args.core/2))
+                
+        if args.memory is not None:
+            if re.match('#\$ -l\s+mt=(\d+)G',line):
+                jobfile[idx]='#$ -l mt={}G\n'.format(args.memory)
 
-    return ""
+        if args.pick is not None:
+            if re.match('pick=([-\d]+)',line):
+                jobfile[idx]='pick={}\n'.format(args.pick)
+            
+
+    # read the info
+    nones = lambda n: [None for _ in range(n)]
+    queue,core,memory,pick=nones(4)
+
+    for idx, line in enumerate(jobfile):
+        mobj=re.match('#\$ -q\s+([.\w]+)',line)
+        if mobj:
+            queue=mobj.group(1)    
+        mobj=re.match('#\$ -pe\s+(\d+)cpn\s+(\d+)',line)
+        if mobj:
+            core=mobj.group(2)
+        mobj=re.match('#\$ -l\s+mt=(\d+)G',line)
+        if mobj:
+            memory=mobj.group(1)    
+        mobj=re.match('pick=([-\d]+)',line)
+        if mobj:
+            pick=mobj.group(1)    
+
+    # write the job file
+    with open(jobname, 'w') as f:
+        for line in jobfile:
+            f.write("{}".format(line))
+
+       
+    ss='queue={}, core={}, memory={}G, pick={}'.format(queue,core,memory,pick)    
+    return ss
+
 
 ######
 def main(argv):
@@ -97,7 +148,14 @@ def main(argv):
 
       -a, --analyze      Analyze the folder instead generate the job file
 
-      -i IDX, --idx IDX  Kill all the jobs with a status
+      -p PICK, --pick PICK   How to pick the next simulaiton
+
+      -q QUEUE, --queue QUEUE  Define a queue: (all.q|UI|INFORMATICS)
+
+      -c CORE, --core CORE  Number of cores
+
+      -m MEMORY, --memory MEMORY   Minimum of memory of the node
+
 
     """
     logging.basicConfig(level=logging.DEBUG,
@@ -110,9 +168,20 @@ def main(argv):
                         action='store_true',default=False,
                         help="Analyze the folder instead generate the job file")
 
-    parser.add_argument('-i', '--idx', action="store", 
-                        type=int,default=0,
-                        help="Kill all the jobs with a status")
+    parser.add_argument('-p', '--pick', action="store", 
+                        type=int,default=None,
+                        help="How to pick the next simulaiton")
+
+    parser.add_argument('-q', '--queue',
+                        default=None,
+                        help="Define a queue: (all.q|UI|INFORMATICS)")
+    parser.add_argument('-c', '--core', type=int,
+                        default=None,
+                        help="Number of cores")
+    parser.add_argument('-m', '--memory', type=int,
+                        default=None,
+                        help="Minimum of memory of the node")
+
     argv.pop(0)
     # logging.debug(argv)
     args = parser.parse_args(argv)
@@ -120,91 +189,14 @@ def main(argv):
     
     if args.analyze:
         # analyze the folder, return the next simulation name
-        ss=analyze_mph_files(args.idx)
+        ss=analyze_mph_files(args.pick)
     else:
         # generate job file
-        ss=generate_job_file()
+        ss1=generate_job_file(args)
+        ss2=analyze_mph_files(0)
+        ss=ss1+'\n'+ss2
     return ss
-
-
-
-
-# def main(argv):
-#     shutil.copy2('/Users/wdai11/bin/dwt-comsol-job-file.job','./dwt-comsol.job')
-#     print('copy dwt-comsol.job')
-#     with open('dwt-comsol.job', 'r') as f:
-#         jobfile=f.readlines()
-
-
-#     for idx, line in enumerate(jobfile):
-#         if re.search('COMSOLCOMSOL',line):
-#             break
-#     jobfile.pop(idx)
  
-    # for mph in mphfiles:
-    #     mph2=re.sub(".mph$","_Done.mph",mph)
-    #     mph_status=mph2+'.status'
-    #     mph_recovery=mph2+'.recovery'
-    #     logfile=re.sub(".mph$","_Log_${num}.txt",mph)
-    #     if   (mph2 in mphfiles or re.search("_Done.mph",mph)):
-    #         print("{} is finished".format(mph))
-    #     else:
-    #         print("Process {}".format(mph))
-    #     # if code
-    #     ss="if [ -f {} ]; then\n".format(mph2)
-    #     jobfile.insert(idx,ss)
-    #     idx+=1
-    #     # done file exist
-    #     ss='  echo "{} exist" '.format(mph2)
-    #     ss+=" >>${recordname}\n"
-    #     jobfile.insert(idx,ss)
-    #     idx+=1
-    #     # else
-    #     ss='else\n'
-    #     jobfile.insert(idx,ss)
-    #     idx+=1
-    #     # file don't exist, begin to run
-    #     ss='  echo "{} to run" '.format(mph2)
-    #     ss+=" >>${recordname}\n"
-    #     jobfile.insert(idx,ss)
-    #     idx+=1
-        # ss="  comsol -nn 1 -np 28 batch "
-        # ss+=" -inputfile {} ".format(mph)
-        # ss+=" -outputfile {} ".format(mph2)
-        # ss+=" -batchlog {} ".format(logfile)
-        # ss+=" >>${recordname}\n"
-        # jobfile.insert(idx,ss)
-        # idx+=1
-        # ss='  rm -v {}\n'.format(mph_status)
-        # jobfile.insert(idx,ss)
-        # idx+=1
-        # ss='  rm -v {}\n'.format(mph_recovery)
-        # jobfile.insert(idx,ss)
-        # idx+=1
-        # # if end
-        # ss='fi\n\n'
-        # jobfile.insert(idx,ss)
-        # idx+=1
-        # ss='echo "===============================" >> ${recordname}\n'
-        # jobfile.insert(idx,ss)
-        # idx+=1
-        # ss='date >> ${recordname}\n\n'
-        # jobfile.insert(idx,ss)
-        # idx+=1
-
-
-    #     res=subprocess.check_output(["unique-number"]).decode("utf-8").rstrip()
-  
-    # for idx, line in enumerate(jobfile):
-    #     line=re.sub("CURRENTDIRECTORY", os.getcwd(), line)
-    #     line=re.sub("MYJOBNAME", "dwt-comsol-"+res, line)
-    #     jobfile[idx]=line
-
-
-    # with open('dwt-comsol.job', 'w') as f:
-    #     for line in jobfile:
-    #         f.write("{}".format(line))
-
 
 
 #########################
